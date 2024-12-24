d97a7fa978e25331131bba510317557d922909fc
Dan-Kechik <ya.dan.kechik@ya.ru>
2024-12-24 17:32:28 +0300

    Print

--- flask_server.py
+++ flask_server.py
@@ -34,18 +34,23 @@ def receive_data():
     
     
     enevt_type = metadata['form'].get('event', None) or 'unknown event'
-    
+    print('-----')
+    print(enevt_type)
     if client_name and client_phone:
         send_str = f'Name: {client_name}; Phone:{client_phone}'
     else:
         send_str = 'Obtained request without valid arguments'
+    print(send_str)
     send_str = '  '.join((enevt_type, send_str))
-    
+    print(send_str)
     api_key = os.environ['MTS_API_KEY']
     crm_phone = os.environ['CRM_ACCOUNT_PHONE']
     mng_phone = os.environ['MANAGER_PHONE']
     payload = {'number': crm_phone, 'destination': mng_phone, 'text': send_str}
+    print(payload)
     r = requests.post(r'https://api.exolve.ru/messaging/v1/SendSMS', headers={'Authorization': 'Bearer '+api_key}, data=json.dumps(payload))
+    print('ANS:')
+    print(r.text)
 
     return str(metadata)+f'\n\n---TEXT: '+r.text, 200
 
